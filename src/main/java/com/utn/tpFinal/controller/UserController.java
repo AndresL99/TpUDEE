c1a9b1b43e14f564f86a6502b8b1fa6bf7c3f127
Andres Lerner <andreslerner99@gmail.com>
2021-05-06 01:29:30 -0300

    Initial commit

--- src/main/java/com/utn/tpFinal/controller/UserController.java
+++ src/main/java/com/utn/tpFinal/controller/UserController.java
@@ -1,6 +1,19 @@
 package com.utn.tpFinal.controller;
 
+import com.utn.tpFinal.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/User")
 public class UserController
 {
+    @Autowired
+    private UserService userService;
+
+
 
 }
