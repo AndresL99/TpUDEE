2edd53187bbb133a05c462b49567c05646e46e11
Andres Lerner <andreslerner99@gmail.com>
2021-05-19 12:37:49 -0300

    Initial commit

--- src/Schema/Scripts/TpUEE_script.sql
+++ src/Schema/Scripts/TpUEE_script.sql
@@ -3,36 +3,54 @@ use UEEApp;
 
 drop database UEEApp;
 create table if not exists users(
-user_name varchar(30) not null,
+id_user integer not null auto_increment,
+user_name varchar(30) not null unique,
 password varchar(30) not null,
-constraint pk_user_name primary key (user_name)
+constraint pk_id_user primary key (id_user)
 );
 
 create table if not exists admins(
-user_name varchar(30),
+id_user integer not null,
 dni_admin integer not null,
-email_admin varchar(50) not null,
+email_admin varchar(50) not null unique,
 first_name_admin varchar(30) not null,
 last_name_admin varchar(30) not null,
-constraint fk_user_name_admin foreign key (user_name) references users (user_name),
+constraint fk_id_admin foreign key (id_user) references users (id_user),
 constraint pk_dni_admin primary key (dni_admin)
 );
 
 create table if not exists clients(
-user_name varchar(30),
+id_user integer not null,
 dni_client integer not null,
-email_client varchar(50) not null,
+email_client varchar(50) not null unique,
 first_name_client varchar(30) not null,
 last_name_client varchar(30) not null,
-constraint fk_user_name_client foreign key (user_name) references users (user_name),
+constraint fk_id_client foreign key (id_user) references users (id_user),
 constraint pk_dni_client primary key (dni_client)
 );
 
-create table if not exists meters(
-serial_number varchar(40) not null,
-model varchar(30) not null,
+create table if not exists brands(
+id_brand integer not null auto_increment,
 brand varchar(30) not null,
-constraint pk_serial_number primary key (serial_number)
+constraint pk_id_brand primary key (id_brand)
+);
+
+create table if not exists models(
+id_model integer not null auto_increment,
+id_brand integer not null,
+model varchar(30) not null,
+serial_number varchar(50) not null,
+constraint pk_id_model primary key (id_model),
+constraint fk_id_brand_model foreign key (id_brand) references brands (id_brand)
+);
+
+create table if not exists meters(
+id_meter integer not null auto_increment,
+id_brand integer not null,
+id_model integer not null,
+constraint pk_id_meter primary key (id_meter),
+constraint fk_id_brand foreign key (id_brand) references brands (id_brand),
+constraint fk_id_model foreign key (id_model) references models (id_model)
 );
 
 create table if not exists tariffs(
@@ -53,26 +71,37 @@ create table if not exists residences(
 id_residence integer not null auto_increment,
 dni_client integer,
 id_address integer,
-serial_number varchar(40),
 id_tariff integer,
 constraint fk_dni_client foreign key (dni_client) references clients(dni_client),
 constraint fk_id_address foreign key (id_address) references addresses(id_address),
-constraint fk_serial_number foreign key (serial_number) references meters(serial_number),
 constraint fk_id_tariff foreign key (id_tariff) references tariffs(id_tariff),
 constraint pk_id_residence primary key (id_residence)
+);
+
+create table if not exists measurements(
+id_measurement integer not null auto_increment,
+id_model integer not null,
+measurement_date date not null,
+kwh_measurement integer not null,
+value float not null,
+password varchar(30) not null,
+constraint pk_id_measurement primary key (id_measurement),
+constraint fk_id_model_measurement foreign key (id_model) references models (id_model)
 );
 
 create table if not exists invoices(
 id_residence integer,
 id_invoice integer auto_increment,
+id_measurement integer not null,
 is_paid boolean not null,
 due_date datetime not null,
 first_read float not null,
 last_read float not null,
 total_cons_kw float not null,
-initial_date datetime not null,
-last_date datetime not null,
+initial_date date not null,
+last_date date not null,
 total_amount float not null,
 constraint fk_id_residence foreign key(id_residence) references residences(id_residence),
+constraint fk_id_measurement foreign key (id_measurement) references measurements (id_measurement),
 constraint pk_id_invoice primary key (id_invoice)
 );
